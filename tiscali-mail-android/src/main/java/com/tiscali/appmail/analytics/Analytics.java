83d5a8dfe6098663c39d08ebe4acaab9386b4b60
Andrea Putzu <aputzu@it.tiscali.com>
2017-02-17 17:45:43 +0100

    working on webtrekk

---
+++ tiscali-mail-android/src/main/java/com/tiscali/appmail/analytics/Analytics.java
@@ -0,0 +1,8 @@
+package com.tiscali.appmail.analytics;
+
+/**
+ * Created by andreaputzu on 17/02/17.
+ */
+
+public class Analytics {
+}
---
+++ tiscali-mail-android/src/test/java/com/tiscali/appmail/notification/TestNotificationTest.java
@@ -0,0 +1,8 @@
+package com.tiscali.appmail.notification;
+
+/**
+ * Created by andreaputzu on 17/02/17.
+ */
+
+public class TestNotificationTest {
+}
